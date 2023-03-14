f7efea047aac40c2f14d35a2d0642410c64476ed
Sahil Bhalla <s2bhalla@ucsd.edu>
2023-03-14 14:42:14 -0700

    Fixied skel

--- Assets/FluidController.cs
+++ Assets/FluidController.cs
@@ -19,10 +19,20 @@ public class FluidController : MonoBehaviour
     RenderTexture oldvelocity;
     RenderTexture scalars1;
     RenderTexture oldscalars1;
+    RenderTexture pressure;
+    RenderTexture oldpressure;
+    RenderTexture forces;
+    //RenderTexture forcemag;
+    RenderTexture densesource;
 
-    int simd;
-    int simv;
+
     int init;
+    int appforce;
+    int jacobv;
+    int jacobp;
+    int projectv;
+    int advel;
+    int adddense;
 
     float dt;
 
@@ -39,24 +49,54 @@ public class FluidController : MonoBehaviour
         density.Create();
 
         olddensity = new RenderTexture(density);
-        olddensity.enableRandomWrite = true;
+        olddensity.enableRandomWrite = false;
         olddensity.Create();
+
+        densesource = new RenderTexture(density);
+        densesource.enableRandomWrite = false;
+        densesource.Create();
 
         velocity = new RenderTexture(settings);
         velocity.enableRandomWrite = true;
+        velocity.graphicsFormat = UnityEngine.Experimental.Rendering.GraphicsFormat.R32G32B32A32_SFloat;
         velocity.Create();
 
         oldvelocity = new RenderTexture(velocity);
-        oldvelocity.enableRandomWrite = true;
+        oldvelocity.enableRandomWrite = false;
+        oldvelocity.graphicsFormat = UnityEngine.Experimental.Rendering.GraphicsFormat.R32G32B32A32_SFloat;
         oldvelocity.Create();
 
-        scalars1 = new RenderTexture(settings);
+        scalars1 = new RenderTexture(velocity);
         scalars1.enableRandomWrite = true;
+        scalars1.graphicsFormat = UnityEngine.Experimental.Rendering.GraphicsFormat.R32G32B32A32_SFloat;
         scalars1.Create();
 
         oldscalars1 = new RenderTexture(scalars1);
-        oldscalars1.enableRandomWrite = true;
+        oldscalars1.enableRandomWrite = false;
+        oldscalars1.graphicsFormat = UnityEngine.Experimental.Rendering.GraphicsFormat.R32G32B32A32_SFloat;
         oldscalars1.Create();
+
+        pressure = new RenderTexture(velocity);
+        pressure.enableRandomWrite = true;
+        pressure.graphicsFormat = UnityEngine.Experimental.Rendering.GraphicsFormat.R32_SFloat;
+        pressure.Create();
+
+        oldpressure = new RenderTexture(pressure);
+        oldpressure.enableRandomWrite = false;
+        oldpressure.graphicsFormat = UnityEngine.Experimental.Rendering.GraphicsFormat.R32_SFloat;
+        oldpressure.Create();
+
+        forces = new RenderTexture(velocity);
+        forces.enableRandomWrite = false;
+        forces.graphicsFormat = UnityEngine.Experimental.Rendering.GraphicsFormat.R8G8B8_SRGB;
+        forces.Create();
+
+        /*
+        forcemag = new RenderTexture(velocity);
+        forcemag.enableRandomWrite = false;
+        forcemag.graphicsFormat = UnityEngine.Experimental.Rendering.GraphicsFormat.R16_SFloat;
+        forcemag.Create();
+        */
 
         fog = gameObject.GetComponent<UnityEngine.Rendering.HighDefinition.LocalVolumetricFog>();
         fog.parameters.volumeMask = density;
@@ -65,11 +105,17 @@ public class FluidController : MonoBehaviour
         sim.SetTexture(init, "dense", density, 0);
         sim.SetTexture(init, "scalar", scalars1, 0);
         sim.SetTexture(init, "vel", velocity, 0);
+        sim.SetTexture(init, "press", pressure, 0);
+        sim.SetTexture(init, "densesource", densesource, 0);
+        sim.SetTexture(init, "forces", forces, 0);
+        //sim.SetTexture(init, "forcemag", forcemag, 0);
         sim.Dispatch(init, density.width/4, density.height/4, density.volumeDepth/4);
-        
-        
-        simd = sim.FindKernel("SimulateDensity");
-        simv = sim.FindKernel("SimulateVelocity");
+        appforce = sim.FindKernel("ApplyForce");
+        jacobv = sim.FindKernel("JacobiVisc");
+        jacobp = sim.FindKernel("JacobiPress");
+        projectv = sim.FindKernel("ProjectVel");
+        advel = sim.FindKernel("AdvectVel");
+        adddense = sim.FindKernel("AddvectDense");
 
     }
 
@@ -79,27 +125,12 @@ public class FluidController : MonoBehaviour
     }
     private void FixedUpdate()
     {
+
+        //Advect Velocity
         Graphics.CopyTexture(velocity, oldvelocity);
 
-        sim.SetTexture(simv, "vel", velocity, 0);
-        sim.SetTexture(simv, "velold", oldvelocity, 0);
-        sim.SetTexture(simv, "scalar", scalars1, 0);
-        sim.SetFloat("dt", dt);
-        sim.SetFloat("vis", viscosity);
-        sim.Dispatch(simv, velocity.width / 4, velocity.height / 4, velocity.volumeDepth / 4);
 
 
-        Graphics.CopyTexture(density, olddensity);
-        sim.SetTexture(simd, "dense", density, 0);
-        sim.SetTexture(simd, "denseold", olddensity, 0);
-        sim.SetFloat("dt", dt);
-        sim.Dispatch(simd, density.width / 4, density.height / 4, density.volumeDepth / 4);
-
-
-        fog.parameters.volumeMask = density;
-        RenderTexture.active = density;
-        gameObject.SetActive(false);
-        gameObject.SetActive(true);
     }
 }
     
